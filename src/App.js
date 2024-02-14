56b17cc7fcb39d0c9e186ae52b8685d0812d21ae
Anton Kucher <antonddmedia@gmail.com>
2024-02-14 12:14:27 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { toast, ToastContainer } from 'react-toastify';
 import './App.css';
 
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
+
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/').then((data) => {
+      const message =   data.data.message;
+      toast.success(message);
+    }).catch((e) => {
+      toast.error(e.message)
+    })
+  }, [])
+
+
+
+
+  const handleClick = () => {
+
+    axios.post('http://localhost:3001/login', { login: login, password: password })
+    .then((data) => {
+      const message =  data.data.message;
+      toast.success(message);
+    })
+    .catch((e) =>{
+      console.log(e.response);
+      const status = e.response.status;
+
+      switch (status) {
+        case 404:
+            toast.warning(e.response.data.message)
+          break;
+        case 403:
+            toast.error(e.response.data.message)
+          break;
+      
+        default:
+          break;
+      }
+
+
+      
+    })
+
+
+  }
+
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <input type='text' placeholder='Логин' value={login} onChange={(e) => setLogin(e.target.value)}/>
+        <input type='password' placeholder='Пароль' value={password} onChange={(e) => setPassword(e.target.value)}/>
+        
+        <button onClick={handleClick}>Войти</button> 
+        
+        <ToastContainer />
+
     </div>
   );
 }
